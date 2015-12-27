d0ac9c3451f3f3064d2b954006e98f6d5ddfa734
Maaz Syed Adeeb <maaz.adeeb@sap.com>
2015-12-27 11:54:37 +0530

    Added th movie detail fragment.

--- app/src/main/java/com/udacity/android/maaz/popularmovies/fragment/MovieDetailFragment.java
+++ app/src/main/java/com/udacity/android/maaz/popularmovies/fragment/MovieDetailFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.udacity.android.maaz.popularmovies.R;
 import com.udacity.android.maaz.popularmovies.model.MovieData;
 
@@ -15,6 +18,8 @@ import com.udacity.android.maaz.popularmovies.model.MovieData;
  */
 public class MovieDetailFragment extends Fragment {
 
+    private static final String BASE_POSTER_URI = "http://image.tmdb.org/t/p/w342";
+
     public MovieDetailFragment() {
     }
 
@@ -22,9 +27,21 @@ public class MovieDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+
+        TextView movieTitle = (TextView) rootView.findViewById(R.id.text_movie_title);
+        ImageView posterImage = (ImageView) rootView.findViewById(R.id.image_poster);
+        TextView releaseDate = (TextView) rootView.findViewById(R.id.text_release_date);
+        TextView rating = (TextView) rootView.findViewById(R.id.text_user_rating);
+        TextView plotSynopsis = (TextView) rootView.findViewById(R.id.text_synopsis);
+
         Intent intent = getActivity().getIntent();
         if(intent != null) {
             MovieData movieData = intent.getParcelableExtra(getString(R.string.movie_data));
+            movieTitle.setText(movieData.getOriginalTitle());
+            Picasso.with(getActivity()).load(BASE_POSTER_URI + movieData.getPosterPath()).into(posterImage);
+            releaseDate.setText(movieData.getReleaseDate().split("-")[0]);
+            rating.setText(movieData.getVoteAverage().toString() + " / 10");
+            plotSynopsis.setText(movieData.getOverview());
         }
         return rootView;
 
